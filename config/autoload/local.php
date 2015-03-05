11a388793ca1bd23c029e160d3478516a459b153
Julien Guittard <julien.guittard@me.com>
2015-03-05 19:44:23 +0100

    Updated local config file

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -1,14 +1,14 @@
 <?php
 return array(
     'zend-server' => array(
-        'key' => 'demo', // 'KEY_PLACEHOLDER',
-        'hash' => '05e32a36e4a573ec127f6886910bb6928e76405e7a8fbfbe461a5eba3756e66b', // 'HASH_PLACEHOLDER',
+        'key' => 'KEY_PLACEHOLDER',
+        'hash' => 'HASH_PLACEHOLDER',
     ),
     'zf-oauth2' => array(
         'storage' => 'ZF\\OAuth2\\Adapter\\PdoAdapter',
         'db' => array(
             'dsn_type' => 'PDO',
-            'dsn' => 'mysql:dbname=otg-oauth2;host=127.0.0.1',
+            'dsn' => 'mysql:dbname=oauth2;host=127.0.0.1',
             'username' => 'root',
             'password' => 'root',
         ),
